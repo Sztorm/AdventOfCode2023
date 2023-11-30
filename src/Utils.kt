76ddb56ef02ee6e4ea08d5289f157c71d02a9202
Sztorm <Marcinwicher99@gmail.com>
2023-11-30 23:54:04 +0100

    Scripts cleanup.

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,3 +1,5 @@
+@file:Suppress("unused")
+
 import java.math.BigInteger
 import java.security.MessageDigest
 import kotlin.io.path.Path
@@ -14,8 +16,3 @@ fun readInput(name: String) = Path("src/$name.txt").readLines()
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
-
-/**
- * The cleaner shorthand for printing output.
- */
-fun Any?.println() = println(this)
