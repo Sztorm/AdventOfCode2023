7c892534b164a02dc3a7ed92941ad8080ea9bd35
Sztorm <Marcinwicher99@gmail.com>
2023-12-01 09:29:35 +0100

    Added Day 1 second part solution.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,12 +1,18 @@
 fun main() {
-    val testInput = readInput("Day01_test")
+    printAnswers("Day01", "Day01_test_part1", 142, ::part1)
+    printAnswers("Day01", "Day01_test_part2", 281, ::part2)
+}
 
-    check(part1(testInput) == 142)
+inline fun printAnswers(
+    inputName: String, testInputName: String, expectedTestResult: Int, answerFunc: (List<String>) -> Int
+) {
+    val testInput: List<String> = readInput(testInputName)
 
-    val input = readInput("Day01")
+    check(answerFunc(testInput) == expectedTestResult)
 
-    println(part1(input))
-    println(part2(input))
+    val input: List<String> = readInput(inputName)
+
+    println(answerFunc(input))
 }
 
 fun part1(input: List<String>): Int = input.sumOf { line: String ->
@@ -16,6 +22,45 @@ fun part1(input: List<String>): Int = input.sumOf { line: String ->
     firstDigit * 10 + lastDigit
 }
 
-fun part2(input: List<String>): Int {
-    return input.size
+private val JOINED_DIGIT_LETTERS_TO_DIGITS = mapOf(
+    "oneight" to "18",
+    "twone" to "21",
+    "threeight" to "38",
+    "fiveight" to "58",
+    "sevenine" to "79",
+    "eightwo" to "82",
+    "nineight" to "98",
+)
+
+private val SINGLE_LETTER_DIGITS_TO_DIGITS = mapOf(
+    "one" to "1",
+    "two" to "2",
+    "three" to "3",
+    "four" to "4",
+    "five" to "5",
+    "six" to "6",
+    "seven" to "7",
+    "eight" to "8",
+    "nine" to "9",
+)
+
+private val JOINED_DIGIT_LETTER_REGEX = Regex(
+    "(oneight)|(twone)|(threeight)|(fiveight)|(sevenine)|(eightwo)|(nineight)"
+)
+private val SINGLE_DIGIT_LETTER_REGEX = Regex(
+    "(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)"
+)
+
+fun part2(input: List<String>): Int = input.sumOf { line: String ->
+    val digitizedLine = JOINED_DIGIT_LETTER_REGEX.replace(line) { matchResult ->
+        JOINED_DIGIT_LETTERS_TO_DIGITS.getOrDefault(matchResult.value, "")
+    }.let {
+        SINGLE_DIGIT_LETTER_REGEX.replace(it) { matchResult ->
+            SINGLE_LETTER_DIGITS_TO_DIGITS.getOrDefault(matchResult.value, "")
+        }
+    }
+    val firstDigit: Int = digitizedLine.first { it.isDigit() }.digitToInt()
+    val lastDigit: Int = digitizedLine.last { it.isDigit() }.digitToInt()
+
+    firstDigit * 10 + lastDigit
 }
