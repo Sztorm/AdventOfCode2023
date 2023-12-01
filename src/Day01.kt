c4ae376ff2cbe97cd95483844e0cc1dee3309d3c
Sztorm <Marcinwicher99@gmail.com>
2023-12-01 07:30:27 +0100

    Added Day 1 first part solution.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,7 @@
 fun main() {
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
 
@@ -8,8 +9,11 @@ fun main() {
     println(part2(input))
 }
 
-fun part1(input: List<String>): Int {
-    return input.size
+fun part1(input: List<String>): Int = input.sumOf { line: String ->
+    val firstDigit: Int = line.first { it.isDigit() }.digitToInt()
+    val lastDigit: Int = line.last { it.isDigit() }.digitToInt()
+
+    firstDigit * 10 + lastDigit
 }
 
 fun part2(input: List<String>): Int {
