f828b2974236957d580521d00ec7135fa92a0480
Sztorm <Marcinwicher99@gmail.com>
2023-12-04 11:09:31 +0100

    Added Day 4 second part solution.

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,28 +1,53 @@
 private fun main() {
     printAnswer("Day04", "Day04_test_part1", 13, ::part1)
-    printAnswer("Day04", "Day04_test_part2", 0, ::part2)
+    printAnswer("Day04", "Day04_test_part2", 30, ::part2)
+}
+
+private data class Card(val winningNumbers: Set<Int>, val scratchedNumbers: Set<Int>) {
+    val matchedNumbers: Set<Int> = winningNumbers.intersect(scratchedNumbers)
+
+    companion object {
+        fun parse(text: String): Card {
+            val (_, numbers) = text.split(':')
+            val (winningNumbers, scratchedNumbers) = numbers.split('|')
+            val winningNumberSet = winningNumbers
+                .split(' ')
+                .filter { it.isNotBlank() }
+                .map { it.toInt() }
+                .toSet()
+            val scratchedNumberSet = scratchedNumbers
+                .split(' ')
+                .filter { it.isNotBlank() }
+                .map { it.toInt() }
+                .toSet()
+
+            return Card(winningNumberSet, scratchedNumberSet)
+        }
+    }
 }
 
 private fun part1(input: List<String>): Int = input.sumOf { line: String ->
-    val (_, numbers) = line.split(':')
-    val (winningNumbers, myNumbers) = numbers.split('|')
-    val winningNumberSet = winningNumbers
-        .split(' ')
-        .filter { it.isNotBlank() }
-        .map { it.toInt() }
-        .toSet()
-    val myNumberSet = myNumbers
-        .split(' ')
-        .filter { it.isNotBlank() }
-        .map { it.toInt() }
-        .toSet()
-    val myWinningNumberSet = winningNumberSet.intersect(myNumberSet)
+    val card = Card.parse(line)
     val points =
-        if (myWinningNumberSet.isEmpty()) 0
-        else 1 shl (myWinningNumberSet.size - 1)
+        if (card.matchedNumbers.isEmpty()) 0
+        else 1 shl (card.matchedNumbers.size - 1)
+
     points
 }
 
 private fun part2(input: List<String>): Int {
-    return input.size
+    val cards = input.map { Card.parse(it) }
+    val counts = IntArray(cards.size) { 1 }
+    var total = 0
+
+    for (i in counts.indices) {
+        val card = cards[i]
+        val count = counts[i]
+        total += count
+
+        for (j in 0..<card.matchedNumbers.size) {
+            counts[i + j + 1] += count
+        }
+    }
+    return total
 }
