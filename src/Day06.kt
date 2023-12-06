4c6166ed67386a081faa195acb4e5664fe0fcbba
Sztorm <Marcinwicher99@gmail.com>
2023-12-06 11:24:06 +0100

    Added Day 6 second part solution.

--- src/Day06.kt
+++ src/Day06.kt
@@ -3,39 +3,48 @@ import kotlin.math.floor
 import kotlin.math.sqrt
 
 private fun main() {
-    printAnswer("Day06", "Day06_test_part1", 288, ::part1)
-    printAnswer("Day06", "Day06_test_part2", 0, ::part2)
+    printAnswer("Day06", "Day06_test_part1", 288L, ::part1)
+    printAnswer("Day06", "Day06_test_part2", 71503L, ::part2)
 }
 
 private val NUMBER_REGEX = Regex("([0-9]+)")
 
-private fun part1(input: List<String>): Int {
-    val times: Sequence<Int> = NUMBER_REGEX
-        .findAll(input[0])
-        .map { it.value.toInt() }
-    val distances: Sequence<Int> = NUMBER_REGEX
-        .findAll(input[1])
-        .map { it.value.toInt() }
-    val product: Int = times
-        .zip(distances)
-        .map { (time, distance) ->
-            val preciseTime = time.toDouble()
-            val preciseDistance = distance.toDouble()
-            val delta = sqrt(preciseTime * preciseTime - 4.0 * preciseDistance)
-            val preciseMinimumHeldTimeA: Double = (preciseTime - delta) * 0.5
-            val preciseMinimumHeldTimeB: Double = (preciseTime + delta) * 0.5
-            val epsilon = 0.001
-            val minimumHeldTimeA = ceil(preciseMinimumHeldTimeA + epsilon).toInt()
-            val minimumHeldTimeB = floor(preciseMinimumHeldTimeB - epsilon).toInt()
-            val beatingTimeCount = minimumHeldTimeB - minimumHeldTimeA + 1
+private fun findNumberOfWaysToBeatRecord(maxTime: Long, recordDistance: Long): Long {
+    val preciseTime = maxTime.toDouble()
+    val preciseDistance = recordDistance.toDouble()
+    val delta = sqrt(preciseTime * preciseTime - 4.0 * preciseDistance)
+    val preciseMinimumHeldTimeA: Double = (preciseTime - delta) * 0.5
+    val preciseMinimumHeldTimeB: Double = (preciseTime + delta) * 0.5
+    val epsilon = 0.001
+    val minimumHeldTimeA = ceil(preciseMinimumHeldTimeA + epsilon).toLong()
+    val minimumHeldTimeB = floor(preciseMinimumHeldTimeB - epsilon).toLong()
 
-            beatingTimeCount
-
-        }.reduce { acc, n -> acc * n }
-
-    return product
+    return minimumHeldTimeB - minimumHeldTimeA + 1
 }
 
-private fun part2(input: List<String>): Int {
-    return input.size
+private fun part1(input: List<String>): Long {
+    val maxTimes: Sequence<Long> = NUMBER_REGEX
+        .findAll(input[0])
+        .map { it.value.toLong() }
+    val recordDistances: Sequence<Long> = NUMBER_REGEX
+        .findAll(input[1])
+        .map { it.value.toLong() }
+
+    return maxTimes
+        .zip(recordDistances)
+        .map { (t, d) -> findNumberOfWaysToBeatRecord(t, d) }
+        .reduce { acc, n -> acc * n }
+}
+
+private fun part2(input: List<String>): Long {
+    val maxTime: Long = NUMBER_REGEX
+        .findAll(input[0])
+        .fold("") { acc, matchResult -> acc + matchResult.value }
+        .toLong()
+    val maxDistance: Long = NUMBER_REGEX
+        .findAll(input[1])
+        .fold("") { acc, matchResult -> acc + matchResult.value }
+        .toLong()
+
+    return findNumberOfWaysToBeatRecord(maxTime, maxDistance)
 }
