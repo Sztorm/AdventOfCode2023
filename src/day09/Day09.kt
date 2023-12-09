7d3e86c7cd1ab4d0d00e5cff4f54b81d43857af5
Sztorm <Marcinwicher99@gmail.com>
2023-12-09 12:06:51 +0100

    Added Day 9 second part solution.

--- src/day09/Day09.kt
+++ src/day09/Day09.kt
@@ -4,13 +4,13 @@ import printAnswer
 
 private fun main() {
     printAnswer(day = 9, testPart = 1, 114, ::part1)
-    printAnswer(day = 9, testPart = 2, 0, ::part2)
+    printAnswer(day = 9, testPart = 2, 5, ::part2)
 }
 
 private fun parseHistory(input: String): List<Int> =
     input.split(' ').map { it.toInt() }
 
-private fun findNextValueOf(history: List<Int>): Int {
+private fun findSubHistoriesOf(history: List<Int>): List<List<Int>> {
     val subHistories = ArrayList<List<Int>>()
     subHistories.add(history)
 
@@ -24,6 +24,12 @@ private fun findNextValueOf(history: List<Int>): Int {
         subHistories.add(subHistory)
     } while (!subHistory.all { it == 0 })
 
+    return subHistories
+}
+
+private fun findNextValueOf(history: List<Int>): Int {
+    val subHistories: List<List<Int>> = findSubHistoriesOf(history)
+
     return subHistories.sumOf { it.last() }
 }
 
@@ -34,6 +40,17 @@ private fun part1(input: List<String>): Int {
     return nextValues.sum()
 }
 
+private fun findPreviousValueOf(history: List<Int>): Int {
+    val subHistories: List<List<Int>> = findSubHistoriesOf(history)
+
+    return subHistories
+        .asReversed()
+        .fold(0) { acc, subHistory -> subHistory.first() - acc }
+}
+
 private fun part2(input: List<String>): Int {
-    return input.size
+    val histories: List<List<Int>> = input.map { parseHistory(it) }
+    val nextValues: List<Int> = histories.map { findPreviousValueOf(it) }
+
+    return nextValues.sum()
 }
